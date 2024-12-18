2736bf642d9f5bad4b3c679f4e95ca4fb114d84b
deltoss <mtran0011@outlook.com>
2024-12-18 15:04:19 +1100

    Corne keymap modifications

--- keyboards/crkbd/keymaps/default/config.h
+++ keyboards/crkbd/keymaps/default/config.h
@@ -20,6 +20,43 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
+#define QUICK_TAP_TERM_PER_KEY
+
+#define AUTO_SHIFT_TIMEOUT 160
+#define RETRO_SHIFT 300
+#define AUTO_SHIFT_REPEAT
+#define NO_AUTO_SHIFT_NUMERIC
+
+#define LEADER_PER_KEY_TIMING
+#define LEADER_NO_TIMEOUT
+#define LEADER_TIMEOUT 200
+
+#define COMBO_TERM 30
+#define COMBO_STRICT_TIMER
+#define COMBO_ONLY_FROM_LAYER 0
+
+#define TAPPING_TERM 175 // TAP VS HOLD timing in milliseconds
+// Pick good defaults for enabling home row mods
+#define TAP_CODE_DELAY 5
+#define TAP_HOLD_CAPS_DELAY 0
+#define PERMISSIVE_HOLD
+
+// Mouse key speed and acceleration.
+#undef MOUSEKEY_DELAY
+#define MOUSEKEY_DELAY 0
+#undef MOUSEKEY_INTERVAL
+#define MOUSEKEY_INTERVAL 12
+#undef MOUSEKEY_WHEEL_DELAY
+#define MOUSEKEY_WHEEL_DELAY 0
+#undef MOUSEKEY_MAX_SPEED
+#define MOUSEKEY_MAX_SPEED 3
+#undef MOUSEKEY_TIME_TO_MAX
+#define MOUSEKEY_TIME_TO_MAX 32
+
+// See https://docs.qmk.fm/features/rgb_matrix#rgb-matrix-effects
+#define RGB_MATRIX_DEFAULT_MODE RGB_MATRIX_JELLYBEAN_RAINDROPS
+#define RGB_MATRIX_DEFAULT_HUE 148 // Default color ‘azure’ blue from https://docs.qmk.fm/features/rgblight.
+
 //#define USE_MATRIX_I2C
 
 //#define QUICK_TAP_TERM 0
