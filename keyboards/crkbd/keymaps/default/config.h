7be272934d10345b7926a308d199460659a390ca
deltoss <mtran0011@outlook.com>
2025-06-09 19:05:44 +1000

    Increased mouse sensitivity as it's been feeling too slow.

--- keyboards/crkbd/keymaps/default/config.h
+++ keyboards/crkbd/keymaps/default/config.h
@@ -45,11 +45,11 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #undef MOUSEKEY_DELAY
 #define MOUSEKEY_DELAY 0
 #undef MOUSEKEY_INTERVAL
-#define MOUSEKEY_INTERVAL 12
+#define MOUSEKEY_INTERVAL 10
 #undef MOUSEKEY_WHEEL_DELAY
 #define MOUSEKEY_WHEEL_DELAY 0
 #undef MOUSEKEY_MAX_SPEED
-#define MOUSEKEY_MAX_SPEED 3
+#define MOUSEKEY_MAX_SPEED 4
 #undef MOUSEKEY_TIME_TO_MAX
 #define MOUSEKEY_TIME_TO_MAX 32
 
