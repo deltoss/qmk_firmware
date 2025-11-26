cf0108771170097b3139254c6022a9cabe52a556
deltoss <mtran0011@outlook.com>
2025-11-26 20:07:03 +1100

    Fixed autoshift for C and M keys, and removed LT from X key

--- keyboards/crkbd/keymaps/default/keymap.c
+++ keyboards/crkbd/keymaps/default/keymap.c
@@ -420,6 +420,9 @@ void matrix_scan_user(void) {
 
 bool get_custom_auto_shifted_key(uint16_t keycode, keyrecord_t *record) {
     switch(keycode) {
+        // Mod Tap or layer Tap keycodes so they get autoshifted
+        case LT(NAVIGATIONS_LAYER, KC_M):
+        case LT(NAVIGATIONS_LAYER, KC_C):
         case MT(MOD_LGUI, KC_N):
         case MT(MOD_LALT, KC_R):
         case MT(MOD_LCTL, KC_T):
@@ -614,7 +617,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------|--------|              |--------|--------+--------+--------+--------+--------+--------|
       LT_UNDERSCORE, MT(MOD_LGUI, KC_N), MT(MOD_LALT, KC_R), MT(MOD_LCTL, KC_T), MT(MOD_LSFT, KC_S), KC_G, LT_VW_TABS,       QUIT_APP, KC_Y, MT(MOD_RSFT, KC_H), MT(MOD_RCTL, KC_A), MT(MOD_RALT, KC_E), MT(MOD_RGUI, KC_I), KC_SCLN,
   //|--------+--------+--------+--------+--------+--------|                                |--------+--------+--------+--------+--------+--------|
-      LT(MOUSE_LAYER, KC_ESC), KC_Q, LT(NAVIGATIONS_LAYER, KC_X), LT(NAVIGATIONS_LAYER, KC_M), LT(NAVIGATIONS_LAYER, KC_C), KC_V,        KC_K,    KC_P, KC_COMM,  KC_DOT, KC_SLSH,  LT_OSM,
+      LT(MOUSE_LAYER, KC_ESC), KC_Q, KC_X, LT(NAVIGATIONS_LAYER, KC_M), LT(NAVIGATIONS_LAYER, KC_C), KC_V,        KC_K,    KC_P, KC_COMM,  KC_DOT, KC_SLSH,  LT_OSM,
   //|--------+--------+--------+--------+--------+--------+--------------|  |--------------+--------+--------+--------+--------+--------+--------|
                 MT_WORD_BSPC, MT(MOD_MEH, KC_BSPC), MT(MOD_LSFT, KC_SPC),    LT(NAVIGATIONS_LAYER, KC_ENT), LT(SYMBOLS_LAYER, KC_DEL), LT_WORD_DEL
                                       //`--------------------------------'  `--------------------------------'
